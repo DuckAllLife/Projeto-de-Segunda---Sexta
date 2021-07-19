c9ab723b4910079104abdf114632c05da7a08617
Gabriel <rochagabriellima@gmail.com>
2021-07-19 19:31:49 -0300

    Primeiro Commit

---
+++ projetando/lib/UI/HomePage/HomePage_Screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [Padding( padding:  EdgeInsets.symmetric(vertical: 40,horizontal:300),
+                child: Image.asset('assets/images/CelularAsset.jpg'),),
+              Myte  
+              ],
+                
+              ));
+           Text('Diga Olá Para O Seu Novo App'),
+           Text('Você ),
+                ),
+    );
+  }
+}
