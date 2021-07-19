e544a601c99b4846156f931ae8097eefb81b56b6
Gabriel <rochagabriellima@gmail.com>
2021-07-19 20:26:05 -0300

    Terminando de Colocar o Texts

--- projetando/lib/UI/HomePage/HomePage_Screen.dart
+++ projetando/lib/UI/HomePage/HomePage_Screen.dart
@@ -8,17 +8,32 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [Padding( padding:  EdgeInsets.symmetric(vertical: 40,horizontal:300),
-                child: Image.asset('assets/images/CelularAsset.jpg'),),
-              Myte  
-              ],
-                
-              ));
-           Text('Diga Olá Para O Seu Novo App'),
-           Text('Você ),
-                ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 40, horizontal: 300),
+              child: Image.asset(
+                'assets/images/CelularAsset.jpg',
+                height: 120,
+                width: 120,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 30, horizontal: 248),
+              child: Text(
+                'Diga Olá Para O Seu Novo App',
+                style: TextStyle(
+                    color: Colors.amber[500], fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 196),
+              child: Text('Você '),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
--- projetando/lib/main.dart
+++ projetando/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:projetando/UI/HomePage/homepage_Screen.dart';;
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
+    title: 'Projetando',
+    home: Homepage(),
   ));
 }
